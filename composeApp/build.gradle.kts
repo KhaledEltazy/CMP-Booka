2b90e8efdd93f430dd1ddc8f0a92882617826086
khaled <khmeltazy@gmail.com>
2025-10-07 19:21:12 +0300

    implementing BookListAction- BookListScreen- BookListState-BookListViewModel and data class Book

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,154 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.sqldelightPlugin)
+    alias(libs.plugins.kotlinSerialization)
+}
+
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+    
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+    
+    jvm()
+    
+    sourceSets {
+        androidMain.dependencies {
+            implementation(compose.preview)
+            implementation(libs.androidx.activity.compose)
+            // Ktor Android engine
+            implementation(libs.ktor.client.okhttp)
+
+            // SQLDelight Android driver
+            implementation(libs.sqldelight.android.driver)
+
+            // Coil (Android + Compose)
+            implementation(libs.coil.compose)
+            implementation(libs.coil.okhttp)
+
+            // Koin Android + Compose
+            implementation(libs.koin.android)
+            implementation(libs.koin.compose)
+        }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodelCompose)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.navigation.compose)
+
+            // Serialization
+            implementation(libs.kotlinx.serialization.core)
+            implementation(libs.kotlinx.serialization.json)
+
+            // Ktor in common
+            implementation(libs.ktor.core)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.ktor.client.logging)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.client.auth)
+            implementation(libs.ktor.client.websockets)
+            implementation(libs.ktor.resources)
+
+            // SQLDelight runtime in common
+            implementation(libs.sqldelight.runtime)
+
+            // Koin core in common
+            implementation(libs.koin.core)
+
+            //coil
+            implementation(libs.coil.compose)
+            implementation(libs.coil.mp)
+            implementation(libs.coil.network.ktor3)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+        jvmMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation(libs.kotlinx.coroutinesSwing)
+
+            // Ktor client for desktop
+            implementation(libs.ktor.client.cio)
+        }
+        iosMain.dependencies {
+            // SQLDelight native driver
+            implementation(libs.sqldelight.native.driver)
+            // (Ktor uses default engine for native via ktor-client-cio or Darwin)
+            implementation(libs.ktor.client.cio)
+        }
+    }
+}
+
+sqldelight {
+    databases {
+        create("AppDatabase") {
+            packageName.set("org.cmp.cmp_booka.cache")
+        }
+    }
+}
+
+android {
+    namespace = "org.cmp.cmp_booka"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "org.cmp.cmp_booka"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    debugImplementation(compose.uiTooling)
+}
+
+compose.desktop {
+    application {
+        mainClass = "org.cmp.cmp_booka.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "org.cmp.cmp_booka"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/org/cmp/cmp_booka/book/domain/Book.kt
@@ -0,0 +1,15 @@
+package org.cmp.cmp_booka.book.domain
+
+data class Book (
+    val id : String,
+    val title : String,
+    val imgUrl : String,
+    val authors : List<String>,
+    val description : String?,
+    val languages : List<String>,
+    val firstPublishYear : String?,
+    val averageReading : Double?,
+    val ratingCount : Int?,
+    val numPages : Int?,
+    val numEditions : Int
+)
